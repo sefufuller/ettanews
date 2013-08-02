5642b25935e750f1c10767185c44a7bbb48210ad
Sefu Action <sefu@ettanews.com>
2013-08-02 03:21:35 +0000

    added Posting to search

--- bing_search_v3.py
+++ bing_search_v3.py
@@ -1,11 +1,12 @@
 import urllib
 import simplejson
 import requests
+import kaleb
 
 def search(term, search_type):
 	quote = urllib.quote("\'"+term+"\'")
-        app_id="64Wn5jmFDbj7CEcr5VG5MaLqnz3FnDOSIeXyxIADzTA="
-        r = requests.get("https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/%s?Query=%s&$format=json" % (search_type,quote), auth=(app_id, app_id))
+        app_id="1p359jn78P6GP1b1TKw+EvXYwIjPjuly+RbAaga7Jlg="
+        r = requests.get("https://api.datamarket.azure.com/Data.ashx/Bing/Search/%s?$format=json&Query=%s" % (search_type,quote), auth=(app_id, app_id))
         print "done search"
 	l = simplejson.loads(r.content)
 	return l
@@ -17,6 +18,7 @@ def parse_results(results_object):
                 url = node[u'Url']
                 abstract = node[u'Description']
                 date = node[u'Date']
+		kaleb.Post(title+"\n"+url+"\n"+abstract)
                 res.append([title, url, abstract, date])
         return res
 
